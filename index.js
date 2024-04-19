7c5264e9d9a7ba58615f30d55e306f096b920081
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-04-19 19:30:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.italic.underline.yellow("\n\tWelcome to Alizey Word Counter..."));
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: (chalk.underline.italic.cyan("Enter a sentence to count a word... "))
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+//print the array of word 
+console.log(words);
+//print the word count of the sentence 
+console.log(chalk.underline.italic.magenta(`Your sentence word count is ${words.length}`));
+console.log(chalk.bold.italic.underline.yellow("\n\tThankyou for using Alizey word counter..."));
